89e09e230a96a694bf19fe4e955bf647992b5400
Kim <kimberley.quenon.@gmail.com>
2024-09-04 15:47:56 +0200

    DESIGN - Posts Banner

--- assets/js/fadeAnim.js
+++ assets/js/fadeAnim.js
@@ -1,16 +1,26 @@
-const slide = document.getElementById('fade-anim').querySelector('.container');
-const cards = document.querySelectorAll('.card');
+function observeAllSlides() {
+    const slides = document.querySelectorAll('.slide');
 
-const observer = new IntersectionObserver((entries) => {
-if (entries[0].isIntersecting) {
-    let delay = 0;
-    cards.forEach((card, index) => {
-    setTimeout(() => {
-        card.classList.add('card-visible');
-    }, delay);
-    delay += 300;
+    slides.forEach((slide) => {
+
+        const cards = slide.querySelectorAll('.card');
+
+        if (cards.length > 0) { 
+            const observer = new IntersectionObserver((entries) => {
+                if (entries[0].isIntersecting) {
+                    let delay = 0;
+                    cards.forEach((card) => {
+                        setTimeout(() => {
+                            card.classList.add('card-visible');
+                        }, delay);
+                        delay += 300;
+                    });
+                }
+            }, { threshold: 0.5 });
+
+            observer.observe(slide);
+        }
     });
 }
-}, { threshold: 1 });
 
-observer.observe(slide);
+observeAllSlides();
