dc59d59d9b7c6ad5b90169c5fd02b5ece3260fb7
Kim <kimberley.quenon.@gmail.com>
2024-09-19 15:13:37 +0200

    DESIGN - Footer

--- assets/js/keepInTouch.js
+++ assets/js/keepInTouch.js
@@ -1,27 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
     const texts = document.querySelectorAll('#keep-in-touch p');
     let index = 0;
-    let forward = true;
 
     function animateText() {
-        if (forward) {
-            texts[index].classList.remove('opacity-0');
-            texts[index].classList.add('opacity-100');
-            index++;
-            if (index >= texts.length) {
-            forward = false;
-            index = texts.length - 1;
-            }
-        } else {
-            texts[index].classList.remove('opacity-100');
-            texts[index].classList.add('opacity-0');
-            index--;
-            if (index < 0) {
-            forward = true;
-            index = 0;
-            }
+        // Afficher le texte actuel
+        texts[index].classList.remove('opacity-0');
+        texts[index].classList.add('opacity-100');
+
+        // Masquer le texte d'il y a deux cycles
+        let previousIndex = index - 2;
+        if (previousIndex < 0) {
+            previousIndex += texts.length;
+        }
+        texts[previousIndex].classList.remove('opacity-100');
+        texts[previousIndex].classList.add('opacity-0');
+
+        // Passer au texte suivant
+        index++;
+        if (index >= texts.length) {
+            index = 0; // Réinitialiser à la première élément
         }
     }
 
-    setInterval(animateText, 100);
+    setInterval(animateText, 150);
 });
